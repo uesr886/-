a3a6d86be6ddb1ee2531b0089be2056fdfaf027f
@user8886 <1663446974@qq.com>
2024-06-24 23:35:17 +0800

    624

--- aigc/aigc_coze_logo/backend/main.mjs
+++ aigc/aigc_coze_logo/backend/main.mjs
@@ -1,14 +1,27 @@
 import Koa from 'koa'
+import Router from 'koa-router'
 // web 服务就是一个app
 const app = new Koa()
-
+const router = new Router() // 路由实例
 // use 挂载一个服务
 // 函数 ctx 是一个对象，里面有 request 和 response
 // HTTP是基于请求响应的简单协议
-app.use((ctx) => {
-    // ctx.response.body  响应体是hello world
-    ctx.body = 'hello world'
+
+router.post('/logo', ctx => {
+    console.log(ctx.request.body, '////');
+    ctx.body = 'logo'
 })
+
+router.get('/', ctx => {
+    ctx.body = '首页'
+})
+
+app.use(router.routes())
+
+// app.use((ctx) => {
+//     // ctx.response.body  响应体是hello world
+//     ctx.body = 'hello world'
+// })
 // http 协议的提供 
 app.listen(3000, () => {
     console.log('server is running at http://localhost:3000');
