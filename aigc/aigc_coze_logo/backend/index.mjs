15c500116f402d92ee0691dd6371784c25935e9e
@user8886 <1663446974@qq.com>
2024-06-25 14:34:54 +0800

    625第一

--- aigc/aigc_coze_logo/backend/index.mjs
+++ aigc/aigc_coze_logo/backend/index.mjs
@@ -1,36 +1,29 @@
 // 多模态能力
-import OpenAI from 'openai';
+import OpenAI from "openai";
 
-const client = new OpenAI({
-    apiKey: 'sk-VtakEUMHv0P1ZU6ERYlGgc9XXvYeLMMYuaUkAczPF5vqp2DP',
-    // gptsapi 会帮我们做openai的请求转发
-    baseURL: 'https://api.302.ai/v1'
+const client=new OpenAI({
+    apiKey:'sk-wpw49pKFIXuQ8bHJL1wJzUCHME30WCo3a56GF2IkSfESzqXh',
+    //gptsapi会帮我们做apenai的请求转发
+    baseURL:'https://api.302.ai/v1'
 })
 
-// prompt
-// 1.它是一位资深的设计师
-// 2.请它为标题为 title，功能为 app 应用设计一款logo
-// 3.要求 高端大气上档次
-let title = '探探'
-let desc = '陌生人社交，相互点赞的陌生人快速交友。'
-
-// const prompt =`
-// 你是一位资深设计师，
-// 请为标题为${title}，功能为${desc}
-// 的移动app应用设计一款logo，
-// 要求 高端大气上档次。
-const prompt =`
+//prompt
+//1. 它是一位资深的设计师
+//2. 请它为标题为title，功能为app应用设计一款logo
+//3. 要求 高端大气上档次
+let title='探探'
+let desc='陌生人社交，相互点赞的陌生人快速交友。'
+const prompt=`
     A spaceship flying through the universe
 `
-const main = async () => {
-    // 文生图 AIGC
-    const response = await client.images.generate({
-        model: 'dall-e-3',
+
+const main=async()=>{
+    //文生图 AIGC
+    const response=await client.images.generate({
+        model:'dall-e-3',
         prompt,
-        n: 1
+        n:1,
     })
-
-    console.log(response.data[0].url);
+    console.log(response.data[0].url)
 }
-
 main()
--- aigc/aigc_coze_logo/backend/main.mjs
+++ aigc/aigc_coze_logo/backend/main.mjs
@@ -1,28 +1,58 @@
-import Koa from 'koa'
-import Router from 'koa-router'
-// web 服务就是一个app
-const app = new Koa()
-const router = new Router() // 路由实例
-// use 挂载一个服务
-// 函数 ctx 是一个对象，里面有 request 和 response
-// HTTP是基于请求响应的简单协议
-
-router.post('/logo', ctx => {
-    console.log(ctx.request.body, '////');
-    ctx.body = 'logo'
+import koa from 'koa'//后端极简框架
+import Router from 'koa-router'//路由
+import { bodyParser } from '@koa/bodyparser'
+import OpenAI from "openai";
+import cors from '@koa/cors';
+//web 服务就是一个app
+const app = new koa()
+const router = new Router()//路由实例
+const client = new OpenAI({
+  apiKey: 'sk-wpw49pKFIXuQ8bHJL1wJzUCHME30WCo3a56GF2IkSfESzqXh',
+  //gptsapi会帮我们做apenai的请求转发
+  baseURL: 'https://api.302.ai/v1'
 })
-
+//use挂载一个服务
+//函数ctx是一个对象，里面有request和response
+//HTTP就是基于请求相应的简单协议
+app.use(bodyParser())
+app.use(cors())
+router.post('/logo', async (ctx) => {
+  let { title, desc } = ctx.request.body
+  // console.log(ctx.request.body,'////');
+  const prompt = `
+    你是一位资深设计师,
+        请为标题为${title},功能为${desc}的移动app应用设计一款logo,
+        要求如下:
+        1. 拥有现代风
+        2. 具有现代审美
+        
+`
+  try {
+    const response = await client.images.generate({
+      model: 'dall-e-3',
+      prompt,
+      n: 1,
+    })
+    ctx.body = {
+      status: 200,
+      url: response.data[0].url
+    }
+  } catch (error) {
+    ctx.body = {
+      status: 500,
+      error: error.message
+    }
+  }
+})
 router.get('/', ctx => {
-    ctx.body = '首页'
+  ctx.body = '首页'
 })
-
 app.use(router.routes())
-
-// app.use((ctx) => {
-//     // ctx.response.body  响应体是hello world
-//     ctx.body = 'hello world'
+// app.use((ctx)=>{
+//     //ctx.response.body 响应体是hello world
+//     ctx.body='hello world'
 // })
-// http 协议的提供 
+//http 协议的提供
 app.listen(3000, () => {
-    console.log('server is running at http://localhost:3000');
+  console.log('server is running at http://localhost:3000')
 })
