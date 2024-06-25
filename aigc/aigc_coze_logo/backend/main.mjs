3b3042e47fcec231ad9d57ffd1d7a5dace6356f6
@user8886 <1663446974@qq.com>
2024-06-25 21:40:59 +0800

    625第二次

--- aigc/aigc_coze_logo/backend/main.mjs
+++ aigc/aigc_coze_logo/backend/main.mjs
@@ -1,58 +1,69 @@
-import koa from 'koa'//后端极简框架
-import Router from 'koa-router'//路由
+import Koa from 'koa' // 后端简单的框架
+import Router from 'koa-router' // 路由对象
 import { bodyParser } from '@koa/bodyparser'
-import OpenAI from "openai";
-import cors from '@koa/cors';
-//web 服务就是一个app
-const app = new koa()
-const router = new Router()//路由实例
+import cors from '@koa/cors'
+import OpenAI from 'openai'
+
 const client = new OpenAI({
-  apiKey: 'sk-wpw49pKFIXuQ8bHJL1wJzUCHME30WCo3a56GF2IkSfESzqXh',
-  //gptsapi会帮我们做apenai的请求转发
+  apiKey: 'sk-RB1JaINx7bzL39g8ADREx37rF5jb8WbvrRSYinjRwEAcNCKL',
+  // gptsapi 会帮我们做openai的请求转发
   baseURL: 'https://api.302.ai/v1'
 })
-//use挂载一个服务
-//函数ctx是一个对象，里面有request和response
-//HTTP就是基于请求相应的简单协议
+
+// web 服务就是一个app
+const app = new Koa()
+const router = new Router() // 路由实例 
+// use 挂载一个服务
+// 函数 ctx 是一个对象，里面有 request 和 response
+// HTTP是基于请求响应的简单协议
+
 app.use(bodyParser())
 app.use(cors())
-router.post('/logo', async (ctx) => {
-  let { title, desc } = ctx.request.body
-  // console.log(ctx.request.body,'////');
-  const prompt = `
-    你是一位资深设计师,
-        请为标题为${title},功能为${desc}的移动app应用设计一款logo,
-        要求如下:
-        1. 拥有现代风
-        2. 具有现代审美
-        
-`
-  try {
-    const response = await client.images.generate({
-      model: 'dall-e-3',
-      prompt,
-      n: 1,
-    })
-    ctx.body = {
-      status: 200,
-      url: response.data[0].url
+
+router.post('/logo',  async (ctx) => {
+    // console.log(ctx.request.body/s, '////')
+    let { title, desc } = ctx.request.body;
+    // console.log(title, desc);
+    const prompt = `
+    你是一位资深设计师，
+    请为标题为 ${title}， 功能为 ${desc} 
+    的移动app应用设计一款logo，
+    要求 高端大气上档次。
+    `
+    try {
+        const response = await client.images.generate({
+            model: 'dall-e-3',
+            prompt,
+            n:1
+        })
+
+        ctx.body = {
+            status: 200,
+            url: response.data[0].url
+        }
+    } catch (error) {
+        ctx.body = {
+            status: 500,
+            error: error.message
+        }
     }
-  } catch (error) {
-    ctx.body = {
-      status: 500,
-      error: error.message
-    }
-  }
+    
+
+    
 })
+
 router.get('/', ctx => {
-  ctx.body = '首页'
+    ctx.body = '首页'
 })
+
 app.use(router.routes())
-// app.use((ctx)=>{
-//     //ctx.response.body 响应体是hello world
-//     ctx.body='hello world'
+
+// app.use((ctx) => {
+//     // ctx.response.body  响应体是hello world
+//     ctx.body = 'hello world'
 // })
-//http 协议的提供
+
+// http 协议的提供 
 app.listen(3000, () => {
-  console.log('server is running at http://localhost:3000')
+    console.log('server is running at http://localhost:3000')
 })
--- aigc/aigc_coze_logo/readme.md
+++ aigc/aigc_coze_logo/readme.md
@@ -1,67 +1,19 @@
-# AI全栈开发，24年最火最高薪的方向
-
-- AI全栈
-    - 前后端分离 frontend
-        - bootstrap css 框架 PC 项目可以快速完成页面开发
-        - container row col 页面布局
-        - html5 表单增强
-            - placeholder 输入框提升 用户体验
-            - required 必填项
-        - 前后端交接的时刻
-            - 前后端分离
-            - 后端 在localhost:3000/logo logo生成的接口
-            - 前后端数据交换标准是JSON
-                - JSON keyval 适合做数据的
-            - http协议 接口文档
-                - POST请求
-                - JSON格式的字符串
-            - 请求行 url method http版本
-            - 请求头 Content-Type cookie token ...
-            - 请求体
-    - 后端 backend
-        - 提供路由，用户可以从哪里来？
-            - 实例化router 对象 new Router()
-            - method + path /logo 路径对象
-            - 函数 ctx 
-            - app.use(router.routes())挂载一下
-        
-        - postman
-            - 前后端分离的
-            - http/router/测试接口的可用性
-            - http 请求模拟工具
-                - 下拉了Method 填了url
-                - 请求体
-                - 测试成功 
-
-        - nodemon
-            - npm i -g nodemon全局
-            - nodemon main.mjs 监听文件并重启服务器
-        - @koa/bodyparser
-            - node 这种后端开发，一个个服务慢慢的挂载的方式来实现的
-            - post 请求体接受 @koa/bodyparser
-            - 挂载一下 app.use()
-
-        - 后端开发成功
-            - :3000/logo logo 的JISON数据返回了
-            - 接口开发任务完成
-            - 前端注重用户体验，后端稳定性
-            - status 200 成功
-            - 返回JSON数据
-
-        - 跨域
-            - http://localhost:5500 前端
-            - http://localhost:3000 后端
-            - 域名（淘宝，pdd）端口不一样（不同的服务）
-            - 被CORS 同源策略 警察 blocked
-            - @koa/cors 跨域解决
-                挂载 app.use(cors())
-    - ai server 
-
-- 前端为什么要学习AI？
-    - AI是大势所趋，24年是中国AI元年
-    - 前端的用户体验因AI带来了改变和创新 AIGC是一种用户体验 
-        - coze logo 生成
-            - 传统 文件上传
-            - AIGC 文生图 dalle-3 多模态模型（文本、图片、视频、音频）
-    - gpt4o 集合 （文本、图片、视频、音频）通往AGI之路的模型
-        和硬件结合起来，机器人就来了，AGI的世界
+{
+  "name": "backend",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "@koa/bodyparser": "^5.1.1",
+    "@koa/cors": "^5.0.0",
+    "koa": "^2.15.3",
+    "koa-router": "^12.0.1",
+    "openai": "^4.52.0"
+  }
+}
