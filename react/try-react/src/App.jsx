c3749883c5ff13680d2b404ee5d687bccc9820f0
@user8886 <1663446974@qq.com>
2024-07-01 21:34:28 +0800

    "71提交"

--- react/try-react/src/App.jsx
+++ react/try-react/src/App.jsx
@@ -1,12 +1,13 @@
 // 根组件 返回JSX的函数就是组件
 import AppHeader from './components/app-header'
 const App = () => {
-  
+  // App 的数据
   const name = "旅梦开发团"
 
   return (
     <div className="container">
-      <AppHeader />
+      {/* 参数传递给子组件 */}
+      <AppHeader name={name}/>
       {/* <h1 className="title">{name}</h1> */}
     </div>
   )
--- react/try-react/src/components/app-header.jsx
+++ react/try-react/src/components/app-header.jsx
@@ -1,8 +1,8 @@
-const AppHeader = () => {
-    const name = "旅梦开发团"
+const AppHeader = (props) => {
+    const {name} = props
   return (
     <div className="app-header">
-      <h1 className="title">{name}</h1>
+      <h1 className="title" theme="day">{name} </h1>
     </div>
   )
 }
