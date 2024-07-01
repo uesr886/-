6d937c1a4b850cc4585602a08f205139b09d5258
@user8886 <1663446974@qq.com>
2024-07-01 21:34:58 +0800

    "大苏打"

--- react/try-react/src/components/app-header.jsx
+++ react/try-react/src/components/app-header.jsx
@@ -2,7 +2,7 @@ const AppHeader = (props) => {
     const {name} = props
   return (
     <div className="app-header">
-      <h1 className="title" theme="day">{name} </h1>
+      <h1 className="title" theme="day">{name} {theme}</h1>
     </div>
   )
 }
